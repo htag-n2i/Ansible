51b4dad60d150941cdffc335f09c80cf48000f93
Jonathan Hurter <jonathan@hurter.fr>
2016-12-01 23:47:53 +0100

    Fix registry

--- roles/builder/templates/deployer.sh
+++ roles/builder/templates/deployer.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
-echo "-> Starting deployer" | tee /var/log/htag/deploy.log
+set -e
+repository=run1.htag.fr:5000
+echo "-> Starting deployer" | tee -a /var/log/htag/deploy.log
 commit=$(git rev-parse --short HEAD)
 
-echo "-> Commit $commit" | tee /var/log/htag/deploy.log
+echo "-> Commit $commit" | tee -a /var/log/htag/deploy.log
 
-docker build . --no-cache -t $1:$commit | tee /var/log/htag/deploy.log
+echo "-> Building image"
+docker build -f ./DockerfileCI --no-cache -t $1:$commit . | tee -a /var/log/htag/deploy.log
 
-echo "-> Done" | tee /var/log/htag/deploy.log
+echo "-> Tagging image" | tee -a /var/log/htag/deploy.log
+docker tag $1:$commit $repository/$1:$commit
+echo "-> Push image"
+docker push $repository/$1:$commit
+echo "-> Clean"
+docker rmi $repository/$1:$commit
+echo "-> Done" | tee -a /var/log/htag/deploy.log
+
+echo "-> Pulling image"
